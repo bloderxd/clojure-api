be4d3ab80679db957b1ed74f12fc8f8bdb36f87f
Daniel <wowmegadenis@hotmail.com>
2016-03-26 16:49:34 -0300

    Add right project dependencies

--- project.clj
+++ project.clj
@@ -1,14 +1,18 @@
 (defproject clojure-api "1.0.0-SNAPSHOT"
   :description "REST API example made with closure s2"
   :dependencies [[org.clojure/clojure "1.3.0"]
-                 [compojure "1.1.1"]
-                 [ring/ring-jetty-adapter "0.2.5"]
-                 [ring/ring-json "0.1.2"]
+                 [org.clojure/clojure "1.5.1"]
+                 [ring/ring-core "1.2.1"]
+                 [ring/ring-jetty-adapter "1.2.1"]
+                 [ring/ring-json "0.2.0"]
+                 [compojure "1.1.6"]
                  [c3p0/c3p0 "0.9.1.2"]
                  [org.clojure/java.jdbc "0.2.3"]
                  [com.h2database/h2 "1.3.168"]
                  [cheshire "4.0.3"]]
       :plugins [[lein-ring "0.7.3"]]
-      :ring {:handler clojure-api.api/app}
+      :ring {:handler clojure-api.api/app
+             :nrepl {:start? true
+                     :port 9998}}
       :profiles
       {:dev {:dependencies [[ring-mock "0.1.3"]]}})
